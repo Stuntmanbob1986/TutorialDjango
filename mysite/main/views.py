3460d22ec57e8e0d54771a95b3d20eca2d0d97de
Stuntmanbob <stuntmanbob1986@web.de>
2023-02-16 22:25:34 +0100

    newest

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -12,6 +12,21 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+
+        elif response.POST.get("newItem"):
+            pass
+
     return render(response, "main/list.html", {"ls": ls})
 
 
